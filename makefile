b97f0bfe7d1d01392b71a9e63371f66cc323ebc3
Markus Schäffer <markusschaeffer@gmx.at>
2018-06-06 18:46:52 +0200

    static prefunded account on each node

--- makefile
+++ makefile
@@ -3,7 +3,9 @@
 #https://github.com/ethereum/go-ethereum/wiki/Command-Line-Options
 
 run_full: init_folders netstats bootnode nodes_startup_full
-	  
+	 
+run_without_netstats: init_folders bootnode nodes_startup_full
+ 
 ####################INITIAL INSTALLATION####################
 install_packages:
 	cd scripts/sh; ./install.sh
@@ -39,9 +41,9 @@ genesis_create:
 #$4 path to genesis.json file
 
 node0_startup_full:
-	cd scripts/sh; sudo ./node_startup.sh 0 127.0.0.1 127.0.0.1 ~/Dropbox/UbuntuVM/DropboxShared/diploma-thesis/genesis_json_files/pow/puppeth_default.json
+	cd scripts/sh; sudo ./node_startup.sh 0 127.0.0.1 127.0.0.1 ~/Dropbox/UbuntuVM/DropboxShared/diploma-thesis/genesis_json_files/pow/genesis1.json
 
 node1_startup_full:
-	cd scripts/sh; sudo ./node_startup.sh 1 127.0.0.1 127.0.0.1 ~/Dropbox/UbuntuVM/DropboxShared/diploma-thesis/genesis_json_files/pow/puppeth_default.json
+	cd scripts/sh; sudo ./node_startup.sh 1 127.0.0.1 127.0.0.1 ~/Dropbox/UbuntuVM/DropboxShared/diploma-thesis/genesis_json_files/pow/genesis1.json
 
 nodes_startup_full: node0_startup_full node1_startup_full
--- scripts/sh/create_account.sh
+++ scripts/sh/create_account.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# create account for node 
+# copy existing keystore files in each node for prefunded accounts with always the same address
 # $1: node index
 
 #https://github.com/ethereum/go-ethereum/wiki/Managing-your-accounts
@@ -10,16 +10,22 @@ set -x #echo on
 node_number=$1
 
 cd $ETH_DIR/node-$node_number/
+
 touch node_account_address.txt
 chmod 777 node_account_address.txt
+#write account address to file
+echo "0x5dfe021f45f00ae83b0aa963be44a1310a782fcc" > node_account_address.txt
+
 touch password.txt
 chmod 777 password.txt
+#write password to file
+echo "iloveethereum" > password.txt
 
-#generate new account
-geth --datadir $ETH_DIR/node-$node_number/ --password $ETH_DIR/node-$node_number/password.txt account new > $ETH_DIR/node-$node_number/node_account_address_temp.txt
-
-#store account address to node_account_address.txt
-cd $SHFOLDER
-bash account_getaddress.sh $node_number
-
-
+#copy already existing private keys (keystore folder)
+cd $ETH_DIR
+cd ..
+cd scripts
+cd staticAccounts
+cp -r keystore $ETH_DIR/node-$node_number/
+cd $ETH_DIR/node-$node_number/
+chmod 777 keystore

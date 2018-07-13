f689adaaa32978b790b81a70ebc49e9dc9cf2336
Markus Schäffer <markusschaeffer@gmx.at>
2018-07-13 16:13:24 +0200

    increase max file descriptor placeholder for script

---
+++ scripts/sh/increase_max_file_descriptors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# increasing the amount of maximum file descriptors on the system
+#https://gist.github.com/luckydev/b2a6ebe793aeacf50ff15331fb3b519d
+
+. env.sh
+set -x #echo on
+
+#TODO for amazon ec2 live environment! 
+#1: copy/replace files with modified versions of storage
+#2: restart system
